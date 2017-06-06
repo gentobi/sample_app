7a0fd1d1cd4f186a59ffcef4368c9a1215f1dcd2
tannguyen <nhattannguyen1610@gmail.com>
2017-06-06 11:30:31 +0700

    add email production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,8 @@ Rails.application.configure do
   address:              "smtp.gmail.com",
   port:                 587,
   domain:               "gmail.com",
-  user_name:            "tdlam7412@gmail.com",
-  password:             "tdlam7412",
+  user_name:            "crazytan94@gmail.com",
+  password:             "Pracetamol12",
   authentication:       "plain",
   enable_starttls_auto: true  }
   # Do not dump schema after migrations.
