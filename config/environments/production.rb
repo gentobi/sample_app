db91ebed135e08b51ad3d846626de61715e43985
tannguyen <nhattannguyen1610@gmail.com>
2017-06-06 12:39:26 +0700

    config email server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,20 @@ Rails.application.configure do
   end
   
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'gmail.com',
+  :user_name => ENV['crazytan94@gmail.com'],
+  :password => ENV['Pracetamol12'],
+  :authentication => 'plain',
+  :enable_starttls_auto => true }
+
+=begin
+  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              "smtp.gmail.com",
@@ -96,6 +110,8 @@ Rails.application.configure do
   password:             "Pracetamol12",
   authentication:       "plain",
   enable_starttls_auto: true  }
+=end
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
