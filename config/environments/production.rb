f30c75c420d6b85c5cb2bb16f47cfb7faba6a91b
tannguyen <nhattannguyen1610@gmail.com>
2017-06-06 10:16:29 +0700

    fix email production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,19 +88,16 @@ Rails.application.configure do
   
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-  
+  config.action_mailer.smtp_settings = {
+  address:              "smtp.gmail.com",
+  port:                 587,
+  domain:               "gmail.com",
+  user_name:            "tdlam7412@gmail.com",
+  password:             "tdlam7412",
+  authentication:       "plain",
+  enable_starttls_auto: true  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
