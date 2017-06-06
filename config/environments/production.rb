5b13a9b1a7b4db6c9e7fdfd7497b9ee698a76b42
tannguyen <nhattannguyen1610@gmail.com>
2017-06-06 10:22:45 +0700

    edit production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,6 @@ Rails.application.configure do
   end
   
 
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
